683cecfd87c8af4a811aea5bed1b27c2d91dde43
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-07-04 20:57:41 -0400

    Normalize department names in CategoriesDepartment enum for consistency

--- src/common/enums/categories-departament.enum.ts
+++ src/common/enums/categories-departament.enum.ts
@@ -1,10 +1,10 @@
 export enum CategoriesDepartment {
-    DEPARTAMENTO_INFORMATICA_TELEMATICA = 'Departamento de Informática y Telemática',
-    DEPARTAMENTO_SESION_BIENES= 'Sesión de Bienes',
-    DEPARTAMENTO_CONTABILIDAD_PRESUPUESTO = 'Departamento de Contabilidad y Presupuesto',
-    DEPARTAMENTO_BIENESTAR_SOCIAL = 'Departamento de Bienestar Social',
-    DEPARTAMENTO_NOMINA = 'Departamento de Nómina',
-    DEPARTAMENTO_REGISTRO_CONTROL = 'Departamento de Registro y Control',
-    DEPARTAMENTO_INGRESO_EGRESO = 'Departamento de Ingreso y Egreso',
-    DEPARTAMENTO_PLANIFICACION = 'Departamento de Planificación',
+    DEPARTAMENTO_INFORMATICA_TELEMATICA = 'departamento de informatica y telematica',
+    DEPARTAMENTO_SESION_BIENES= 'sesion de bienes',
+    DEPARTAMENTO_CONTABILIDAD_PRESUPUESTO = 'departamento de contabilidad y presupuesto',
+    DEPARTAMENTO_BIENESTAR_SOCIAL = 'departamento de bienestar social',
+    DEPARTAMENTO_NOMINA = 'departamento de nomina',
+    DEPARTAMENTO_REGISTRO_CONTROL = 'departamento de registro y control',
+    DEPARTAMENTO_INGRESO_EGRESO = 'departamento de ingreso y egreso',
+    DEPARTAMENTO_PLANIFICACION = 'departamento de planificacion',
 }
